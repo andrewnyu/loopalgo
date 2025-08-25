ccc6629fff6b839325b2798f39d7768c8559d28f
Andrew Yu <andrewthomasyu@gmail.com>
2025-08-26 00:10:39 +0800

    Add landing page

--- loopalgo/urls.py
+++ loopalgo/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import RedirectView
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='interview/', permanent=True)),
+    path('', TemplateView.as_view(template_name='landing.html'), name='landing'),
     path('admin/', admin.site.urls),
     path('interview/', include('interview.urls')),
     path('accounts/', include('django.contrib.auth.urls')),

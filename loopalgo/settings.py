ea9449d73b859f9080f0756fa9ad218f6a37ab69
Andrew Yu <andrewthomasyu@gmail.com>
2025-08-27 13:23:49 +0800

    feat(admin): add material design admin interface with Jazzmin
    • Add Jazzmin for modern Material Design admin UI
    • Configure Whitenoise for static file serving
    • Fix static file handling in development and production
    • Add custom branding and icons for admin interface

--- loopalgo/settings.py
+++ loopalgo/settings.py
@@ -19,8 +19,71 @@ ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split('
 # Admin URL configuration
 ADMIN_URL = os.getenv('DJANGO_ADMIN_URL', 'admin/')
 
+# Jazzmin Settings
+JAZZMIN_SETTINGS = {
+    # Title on the login screen
+    "site_title": "ArchipelAlgo Admin",
+    
+    # Title on the brand (top left)
+    "site_header": "ArchipelAlgo",
+    
+    # Square logo to use for your site
+    "site_logo": None,
+    
+    # Welcome text on the login screen
+    "welcome_sign": "Welcome to ArchipelAlgo",
+    
+    # Copyright on the footer
+    "copyright": "ArchipelAlgo",
+    
+    # The model admin to search from the search bar
+    "search_model": "auth.User",
+    
+    # Field name on user model that contains avatar ImageField/URLField/Charfield
+    "user_avatar": None,
+    
+    ############
+    # Top Menu #
+    ############
+    # Links to put along the nav bar
+    "topmenu_links": [
+        {"name": "Home", "url": "admin:index", "permissions": ["auth.view_user"]},
+    ],
+    
+    #############
+    # Side Menu #
+    #############
+    # Whether to display the side menu
+    "show_sidebar": True,
+    
+    # Whether to expand the side menu
+    "navigation_expanded": True,
+    
+    # Custom icons for side menu apps/models
+    "icons": {
+        "auth": "fas fa-users-cog",
+        "auth.user": "fas fa-user",
+        "auth.Group": "fas fa-users",
+        "interview.userprofile": "fas fa-id-card",
+    },
+    
+    # Icons that are used when one is not manually specified
+    "default_icon_parents": "fas fa-chevron-circle-right",
+    "default_icon_children": "fas fa-circle",
+    
+    #############
+    # UI Tweaks #
+    #############
+    # Whether to show the UI customizer on the sidebar
+    "show_ui_builder": False,
+    
+    # Theme
+    "dark_mode_theme": None,
+}
+
 # Application definition
 INSTALLED_APPS = [
+    'jazzmin',  # Material design admin interface
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -32,6 +95,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -88,7 +152,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = []  # Empty since we're not adding any custom static files yet
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- loopalgo/urls.py
+++ loopalgo/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='landing.html'), name='landing'),
     path(settings.ADMIN_URL, admin.site.urls),
     path('interview/', include('interview.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
